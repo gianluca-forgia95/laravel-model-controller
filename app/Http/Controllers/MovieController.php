417a60276e077a1d0a478c632e832acfb8987a77
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-05-13 16:06:01 +0200

    stampo il titolo dei film

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Movie;
+
+class MovieController extends Controller
+{
+    // 
+    public function index() {
+
+        $movies = Movie::all();
+        
+        return view('film' , [
+            'movies' => $movies 
+        ]);
+    }
+}
