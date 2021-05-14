09d5277aa4a72f8a140eca81211449841c3f684d
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-05-14 15:24:26 +0200

    link scss

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -4,17 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Film</title>
+     {{-- File CSS --}}
+     <link rel="stylesheet" href="{{ asset( 'css/app.css' )}}">
+     {{-- /File CSS --}}
+    <title>All Movies List</title>
 </head>
 <body>
-   
+
+ <div class="container">
    
     @foreach ($movies as $movie)
+    <div class="box-movie">
      <h2>Titolo: {{ $movie->title }}</h2>
      <span>Genere: {{ $movie->genre }}</span>
      <h3>Regia: {{ $movie->director }}</h3>
-     <button><a href="{{route('movies.show', [ 'movie' => $movie->id ])}}">Dettaglio film</a></button>
+     <button><a href="{{route('movies.show', [ 'movie' => $movie->id ])}}">Preview</a></button>
+    </div>
     @endforeach
+ 
+</div>
+
 </body>
 </html>
 
--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -4,12 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     {{-- File CSS --}}
+     <link rel="stylesheet" href="{{ asset( 'css/app.css' )}}">
+     {{-- /File CSS --}}
     <title>Movie</title>
 </head>
 <body>
     <div class="container">
         <h2>{{ $movie->title }}</h2>
         <p>{{ $movie->description }}</p>
+        <button><a href="{{route('movies.index')}}">Back To HomePage</a></button>
     </div>
     
 </body>
